5337232934b6ac0275a327362a57f8386e928704
Hayden Gemeinhardt <66012861+haydengemeinhardt@users.noreply.github.com>
2021-11-15 21:25:38 -0600

    Update transition.js

--- js/transition.js
+++ js/transition.js
@@ -1,6 +1,6 @@
 window.onload = () => {
 	const transition_el = document.querySelector('.transition');
-	const anchors = document.querySelectorAll('a, div.bookdescription');
+	const anchors = document.querySelectorAll('a');
 	
 	setTimeout(() => {
 		transition_el.classList.remove('is-active');
