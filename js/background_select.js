2c8470cea9fc6b0c332101d5565a62c01661c9a6
Hayden Gemeinhardt <66012861+haydengemeinhardt@users.noreply.github.com>
2022-01-17 20:14:36 -0600

    Update background_select.js

--- js/background_select.js
+++ js/background_select.js
@@ -2,5 +2,5 @@ window.onload = () => {
 	var backgrounds=['futurecity','futurecity2','futurecity3'];
 	var randn = Math.floor(Math.random() * backgrounds.length);
 	var background = backgrounds[randn];
-	document.body.background = '/img/'+background+'.jpg';
+	document.body.backgroundImage = '/img/'+background+'.jpg';
 }
